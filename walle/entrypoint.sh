dbf04ea1ae30ecf316d28fded8944ca6cc4f7955
captain bu <58945591@qq.com>
2016-07-01 00:24:20 +0800

    UPDATE:  更新启动顺序.

--- walle/entrypoint.sh
+++ walle/entrypoint.sh
@@ -1,13 +1,16 @@
 #!/usr/bin/env sh
 set -e
 
-#init walle
-/opt/walle-web/yii walle/setup --interactive=0
+#start db
+mysqld_safe --user=mysql &
 
 #start php-fpm
 /usr/local/sbin/php-fpm&
 
+#init walle
+/opt/walle-web/yii walle/setup --interactive=0
+
 #start nginx
-/usr/sbin/nginx
+#/usr/sbin/nginx
 
 exec $@

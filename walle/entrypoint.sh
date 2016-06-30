8e7c98a55cbdda84645fc0b6561df7268063ee3f
YoungMan <bushengquan@eastspider.com>
2016-06-30 19:47:01 +0800

    UPDATE:  更新启动命令.

--- walle/entrypoint.sh
+++ walle/entrypoint.sh
@@ -1,6 +1,13 @@
-i#!/usr/bin/env bash
+#!/usr/bin/env sh
 set -e
 
+#init walle
 /opt/walle-web/yii walle/setup --interactive=0
+
+#start php-fpm
+/usr/local/sbin/php-fpm&
+
+#start nginx
+/usr/sbin/nginx
 
 exec $@

22ec8ef25d0572647d70fc59ed6350c29e9b324e
YoungMan <bushengquan@eastspider.com>
2016-07-03 14:54:47 +0800

    UPDATE: 更新环境变量，可以指定server_name.

--- walle/entrypoint.sh
+++ walle/entrypoint.sh
@@ -13,6 +13,11 @@ if [ ! -e /var/www/html/index.php ];then
   echo '?>' >>/var/www/html/index.php
 fi
 
+#replace default server_name
+if [ $SERVER_NAME != "walle.company.com" ];then
+	sed -ri "s/walle.company.com/$SERVER_NAME/" /etc/nginx/nginx.conf
+fi
+
 #start nginx
 /usr/sbin/nginx
 

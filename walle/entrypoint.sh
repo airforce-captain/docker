b1183c8ce4996cfc691daebf6caa623eeefc12f9
YoungMan <bushengquan@eastspider.com>
2017-01-03 20:20:21 +0800

    CHANGELOG: --------------------------------------- UPDATE --------------------------------------- 1. 更新了walle 的docker 镜像，支持添加参数可修改mysql root密码、walle 的数据库密码、以及walle的smtp 自定义配置.
    2. 更新了README说明文件.

--- walle/entrypoint.sh
+++ walle/entrypoint.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env sh
-set -e
+#set -e
+set -x
 
 #inital db
 if [ ! -d "$DATADIR/mysql" ]; then
@@ -26,9 +27,39 @@ fi
 
 WALLE_DB_PASS=${WALLE_DB_PASS:-"walle"}
 
-/usr/bin/mysql -e 'use mysql;update user set password=password("$WALLE_DB_PASS") where user="root";flush privileges'
+/usr/bin/mysql -e "update mysql.user set password=password('${WALLE_DB_PASS}') where user='root';flush privileges"
 
-/usr/bin/mysql -uroot -p${WALLE_DB_PASS} -e 'grant all on *.* to "root"@"%" identified by "$WALLE_DB_PASS";flush privileges;'
+/usr/bin/mysql -uroot -p${WALLE_DB_PASS} -e "grant all on *.* to 'root'@'%' identified by '$WALLE_DB_PASS';flush privileges;"
+
+#change walle connect to DB passwd.
+sed -ri "/WALLE_DB_PASS/ s/(.*:)? (.*)$/\1 '$WALLE_DB_PASS',/" /opt/walle-web/config/local.php
+
+#smtp setup, if want change.
+WALLE_MAIL_HOST=${WALLE_MAIL_HOST:-"smtp.exmail.qq.com"}
+WALLE_MAIL_USER=${WALLE_MAIL_USER:-"service@huamanshu.com"}
+WALLE_MAIL_PASS=${WALLE_MAIL_PASS:-"K84erUuxg1bHqrfD"}
+WALLE_MAIL_PORT=${WALLE_MAIL_PORT:-"25"}
+WALLE_MAIL_ENCRYPTION=${WALLE_MAIL_ENCRYPTION:-"tls"}
+
+for s in WALLE_MAIL_HOST WALLE_MAIL_USER WALLE_MAIL_PASS WALLE_MAIL_PORT WALLE_MAIL_ENCRYPTION
+do
+case $s in
+	*HOST)
+	sed -ri "/$s/ s/(.*:)? (.*)$/\1 '$WALLE_MAIL_HOST',/" /opt/walle-web/config/local.php
+	;;
+	*USER)
+	sed -ri "/$s/ s/(.*:)? (.*)$/\1 '$WALLE_MAIL_USER',/" /opt/walle-web/config/local.php
+	;;
+	*PASS)
+	sed -ri "/$s/ s/(.*:)? (.*)$/\1 '$WALLE_MAIL_PASS',/" /opt/walle-web/config/local.php
+	;;
+	*PORT)
+	sed -ri "/$s/ s/(.*:)? (.*)$/\1 '$WALLE_MAIL_PORT',/" /opt/walle-web/config/local.php
+	;;
+	*ENCRYPTION)
+	sed -ri "/$s/ s/(.*:)? (.*)$/\1 '$WALLE_MAIL_ENCRYPTION',/" /opt/walle-web/config/local.php
+esac
+done
 
 #if no index
 if [ ! -e /var/www/html/index.php ];then

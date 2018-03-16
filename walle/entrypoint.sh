afacb7988674c030bd26f452f16c6a24a64b702e
captain bu <58945591@qq.com>
2018-03-16 19:01:38 +0800

    Update entrypoint.sh

--- walle/entrypoint.sh
+++ walle/entrypoint.sh
@@ -11,21 +11,13 @@ if [ ! -d "$DATADIR/mysql" ]; then
 	mysql_install_db --user=mysql --datadir=$DATADIR
 	mysqld_safe --user=mysql --datadir=$DATADIR&
 	sleep 5
-else
-	if [ ! -d "$DATADIR/walle" ]; then
-		mkdir -p $DATADIR/walle
-		chown -R mysql $DATADIR/walle
-		mysqld_safe --user=mysql --datadir=$DATADIR&
-		sleep 5
-	else
-		mysqld_safe --user=mysql --datadir=$DATADIR&
-		sleep 5
-	fi
 fi
 
 #set mysql root pass,if no change will be use default value.
 
 WALLE_DB_PASS=${WALLE_DB_PASS:-"walle"}
+
+/usr/bin/mysql -uroot -e "create database if not exists walle default charset='utf8mb4';"
 
 /usr/bin/mysql -uroot -e "grant all on *.* to 'root'@'%' identified by '$WALLE_DB_PASS';flush privileges;"
 
@@ -60,14 +52,6 @@ case $s in
 	sed -ri "/$s/ s/(.*:)? (.*)$/\1 '$WALLE_MAIL_ENCRYPTION',/" /opt/walle-web/config/local.php
 esac
 done
-
-#if no index
-if [ ! -e /var/www/html/index.php ];then
-  mkdir -p /var/www/html
-  echo '<?php' >/var/www/html/index.php
-  echo 'phpinfo();' >>/var/www/html/index.php
-  echo '?>' >>/var/www/html/index.php
-fi
 
 #replace default server_name
 if [ $SERVER_NAME != "walle.company.com" ];then
